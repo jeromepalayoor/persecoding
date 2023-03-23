8dc35c6a1ebdb6e63b63b59d19decd6c7eee3405
jeromepalayoor <me@jp-ch.gq>
2023-03-23 14:06:57 +0800

    complete

---
+++ PCTC 2021-22 R0/PCTC 2022 R0 Q3.py
@@ -0,0 +1,7 @@
+userinput = input()
+if userinput == "greet me":
+    print("hello")
+elif userinput == "compliment me":
+    print("you are kind")
+elif userinput == "discuss weather":
+    print("it is treacherous out there")
